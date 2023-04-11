12b1c22388c3804e8cde471108d4d426dfaeeef5
Brian Thorne <brian@thorne.link>
2023-04-11 17:37:14 +1200

    Rename to first_task

--- writemore/cli.py
+++ writemore/cli.py
@@ -10,7 +10,7 @@ class WriteMoreCLISettings(BaseSettings):
     OPENAI_API_KEY: str
 
     OBJECTIVE: Optional[str]
-    TASK: Optional[str]
+    FIRST_TASK: Optional[str]
 
 
 settings = WriteMoreCLISettings()
@@ -24,11 +24,11 @@ settings = WriteMoreCLISettings()
     prompt=True,
     default=lambda: settings.OBJECTIVE,
 )
-@click.option("-t", "--task", help="Task", prompt=True, default=lambda: settings.TASK)
+@click.option("--first-task", help="First task", prompt=True, default=lambda: settings.FIRST_TASK)
 @click.option("-v", "--verbose", is_flag=True, help="Run in verbose mode.")
-def main(objective: str, task: str, verbose: bool):
+def main(objective: str, first_task: str, verbose: bool):
     print(f"Executing a plan for this objective: {objective}")
-    writemore(objective, task, verbose)
+    writemore(objective, first_task, verbose)
 
 
 if __name__ == "__main__":
